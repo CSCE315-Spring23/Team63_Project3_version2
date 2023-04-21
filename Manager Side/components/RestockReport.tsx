97f89397465aa137a0302e6352466bbc0785c2a3
Daniel <djs2002@tamu.edu>
2023-04-21 12:05:51 -0500

    created restock report that prints every item that is needed to be restocked

--- Manager Side/components/RestockReport.tsx
+++ Manager Side/components/RestockReport.tsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+
+interface RestockReportData {
+  item_id: string;
+  ingredient_name: string;
+  quantity: number;
+}
 
 export default function RestockReport() {
+  const [restockData, setRestockData] = useState<RestockReportData[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/report/inventory/restock');
+      // if (response.status >= 200 && response.status < 300) {
+      //   console.log(response.status);
+      //   console.log(response.data);
+      // }
+      // else {
+      //   console.error('request failed with status code ${response.status}');
+      // }
+      setRestockData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleButtonClick = () => {
+    fetchData();
+  };
+
   return (
-    <div>RESTOCK REPORT WILL BE DISPLAYED HERE</div>
-  )
+    <div>
+      <h1>Restock Report</h1>
+      <button onClick={handleButtonClick}>Fetch Data</button>
+      {restockData.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>Item ID</th>
+              <th>Ingredient Name</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {restockData.map((restock) => (
+              <tr key={restock.item_id}>
+                <td>{restock.item_id}</td>
+                <td>{restock.ingredient_name}</td>
+                <td>{restock.quantity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
