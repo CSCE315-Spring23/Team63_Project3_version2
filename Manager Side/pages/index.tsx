b07c222825df202345cc0e24edb915603a5d5272
Ayo Fatoye <afatoye18@gmail.com>
2023-04-30 22:26:18 -0500

    merged all my changes to the main. after this, the weather api and google translate should be working

--- Manager Side/pages/index.tsx
+++ Manager Side/pages/index.tsx
@@ -1,22 +1,7 @@
-/*export default function Home() {
+export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
   )
-}*/
-
-
-import React from 'react';
-import GoogleAuth from '../pages/GoogleAuth';
-
-const HomePage: React.FC = () => {
-  return (
-    <div>
-      <h1>Welcome to My App</h1>
-      <GoogleAuth />
-    </div>
-  );
-};
-
-export default HomePage;
+}
